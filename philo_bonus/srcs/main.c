c1615d9b6f592390f55fbf02e624c6817d6929ab
sseyen <krisstalexx@gmail.com>
2025-04-28 14:53:13 +0200

    Unlink semaphores in the beginning

--- philo_bonus/srcs/main.c
+++ philo_bonus/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 14:19:07 by alisseye          #+#    #+#             */
-/*   Updated: 2025/04/28 13:03:36 by alisseye         ###   ########.fr       */
+/*   Updated: 2025/04/28 14:48:44 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int	main(int argc, char **argv)
 	sem_t	*forks;
 	t_sim	sim;
 
+	sem_unlink("/print");
+	sem_unlink("/meal");
+	sem_unlink("/forks");
 	if (!parse_args(argc, argv, &sim))
 		return (1);
 	if (!init_sems(&sim, &forks))
